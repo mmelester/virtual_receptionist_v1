1376a1271cf8457cfd19f55eefc4fa549f95a1dc
mmelester <mtmelester@gmail.com>
2024-12-06 12:48:09 -0600

    Finish change model file name

--- router.js
+++ router.js
@@ -5,17 +5,17 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('./controllers/homeController');
 const adminController = require('./controllers/adminController');
-const CompanyModel = require('./models/CompanyModel'); // Import the AdminView model
+const CompanyModel = require('./models/CompanyModel'); // Import the CompanyModel
 
 module.exports = (db) => {
-    const adminView = new CompanyModel(db); // Initialize AdminView with the database
+    const companyList = new CompanyModel(db); // Initialize CompanyModel with the database
 
     // Routes
     router.get('/', homeController.home); // Home route
 
     // Admin routes
-    router.get('/admin', (req, res) => adminController.companies(req, res, adminView)); // Fetch companies
-    router.post('/admin/add', (req, res) => adminController.addCompany(req, res, adminView)); // Add a company
+    router.get('/admin', (req, res) => adminController.companies(req, res, companyList)); // Fetch companies
+    router.post('/admin/add', (req, res) => adminController.addCompany(req, res, companyList)); // Add a company
 
     return router; // Export the router
 };
