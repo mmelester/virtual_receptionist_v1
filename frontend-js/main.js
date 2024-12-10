8eb8aa6223690fe5a5b9987097eee212eea3c300
mmelester <mtmelester@gmail.com>
2024-12-10 15:27:20 -0600

    Add Add New Company button and create-company.js to unhide add-company section

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,2 +1,3 @@
 import DragNDrop from './modules/drag-n-drop'
 import GetFormInfor from './modules/getFormInfo'
+import CreateCompany from './modules/create-company'
---
+++ frontend-js/modules/create-company.js
@@ -0,0 +1,12 @@
+// Get the button and section elements
+const createCompanyButton = document.getElementById('create-company-btn');
+const addCompanySection = document.getElementById('add-company-section');
+
+// Add click event listener to the button
+createCompanyButton.addEventListener('click', function () {
+  // Hide the button
+  createCompanyButton.style.display = 'none';
+
+  // Remove the 'd-none' class from the section
+  addCompanySection.classList.remove('d-none');
+});
