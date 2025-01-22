b330cd76eb46f52ccf4f117289f9d7c4d5ee7a82
mmelester <mtmelester@gmail.com>
2025-01-22 15:44:56 -0600

    WIP - baseline working

--- controllers/buildingController.js
+++ controllers/buildingController.js
@@ -13,22 +13,26 @@ module.exports = {
                 // Update the existing record
                 const result = await BuildingModel.updateBuilding(existingBuilding._id, { name, intro, image });
                 if (result.success) {
-                    return res.status(200).json({ success: true, message: 'Building updated successfully!' });
+                    return req.session.save(() => res.status(200).json({ success: true, message: 'Building updated successfully!' }));
                 } else {
-                    return res.status(400).json({ success: false, message: 'Failed to update the building.' });
+                    req.flash('errors', [result.message]);
+                    return req.session.save (() =>res.status(400).json({ success: false, message: 'Failed to retrieve the building record.' }));
                 }
             } else {
                 // Create a new record
                 const result = await BuildingModel.addBuilding({ name, intro, image });
                 if (result.success) {
-                    return res.status(200).json({ success: true, message: 'Building created successfully!' });
+                    req.flash('success', 'Building record added successfully!');
+                    return req.session.save(() => res.status(200).json({ success: true, message: 'Building added successfully!' }));
                 } else {
-                    return res.status(400).json({ success: false, message: 'Failed to create the building.' });
+                    req.flash('errors', [result.message]);
+                    return req.session.save(() => res.status(400).json({ success: false, message: result.message }));
                 }
             }
         } catch (error) {
             console.error('Error saving building:', error);
-            res.status(500).json({ success: false, message: 'An unexpected error occurred.' });
+            req.flash('errors', ['Failed to add company.']);
+            req.session.save(() => res.status(500).json({ success: false, message: 'An unexpected error occurred on the server.' }));
         }
     },
 
