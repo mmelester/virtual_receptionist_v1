538d14fc2e63e00bdfd23db2092e5323791bff03
mmelester <mtmelester@gmail.com>
2025-02-08 17:40:28 -0600

    Add create-user.js with logic and add create user button without logic

---
+++ frontend-js/modules/create-user.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button and section elements
+    const createUserButton = document.getElementById('create-user-btn');
+    const addUserSection = document.getElementById('add-user-section');
+
+    // Add click event listener to the button
+    createUserButton.addEventListener('click', function () {
+        // Hide the button
+        createUserButton.classList.add('d-none');
+        // Set heading
+        addUserSection.classList.remove('d-none');
+    });
+});
+
