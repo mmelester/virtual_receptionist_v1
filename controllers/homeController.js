58717b298f686552d539e9f9859c05c5b409d49d
mmelester <mtmelester@gmail.com>
2025-02-12 13:09:24 -0600

    Fix header logo not appearing on public pages

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,15 +1,19 @@
+const BuildingModel = require('../models/BuildingModel');
 const CompanyModel = require('../models/CompanyModel');
 
 exports.home = async function (req, res, db) {
+    const buildingModel = new BuildingModel(db);
     const companyModel = new CompanyModel(db);
     // Check if the user is logged in
     const isLoggedIn = req.session && req.session.isLoggedIn;
     console.log("Logged in ? ", isLoggedIn)
 
     try {
+        const building = await buildingModel.getBuilding();
         const companies = await companyModel.getCompanies();
 
         res.render('home/index.ejs', { 
+            building, 
             companies, 
             error: null, 
             isLoggedIn // Pass isLoggedIn to the view
@@ -17,11 +21,12 @@ exports.home = async function (req, res, db) {
     } catch (error) {
         console.error('Error retrieving companies:', error);
         res.render('home/index.ejs', { 
+            building: null,
             companies: [], 
-            error: 'Failed to load companies.', 
+            error: 'Failed to load companies and/or building records.', 
             isLoggedIn // Pass isLoggedIn to the view even in case of errors
         });
         req.flash('errors', error)
-        req.session.save(() => res.status(500).json({ success: false, message: 'Failed to fetch company information from database.' }));
+        req.session.save(() => res.status(500).json({ success: false, message: 'Failed to fetch company and/or building information from database.' }));
     }
 };
